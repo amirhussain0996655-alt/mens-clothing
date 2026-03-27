8bf9dfc14c846ae55c35dc301f6aabc96224d714
amirhussain0996655-alt <amir.hussain0996655@gmail.com>
2026-03-27 14:32:43 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{padding:20,fontFamily:'Arial'}}>
+      <h1>MenStyle Store</h1>
+      <h2>Products</h2>
+      <ul>
+        <li>White Shirt - ₹999</li>
+        <li>Denim Jacket - ₹2499</li>
+        <li>T-Shirt - ₹699</li>
+        <li>Blazer - ₹3999</li>
+      </ul>
+      <button>Shop Now</button>
+    </div>
+  )
+}
